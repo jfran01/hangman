5228555be56015ecd9e36a0e0bf7d3b9de23b39b
Juliette <211356692+jfran01@users.noreply.github.com>
2026-06-17 17:38:50 +0100

    Check guess against secret word and allow gameplay

--- lib/Game.rb
+++ lib/Game.rb
@@ -1,18 +1,30 @@
+require_relative "Display"
+require "pry-byebug"
+
 class Game 
   attr_reader :secret
 
   def initialize
-    @secret = File.readlines("google-10000-english-usa-no-swears-long.txt").sample
+    @display = Display.new
+    @secret = File.readlines("google-10000-english-usa-no-swears-long.txt").sample.chomp
     @guessed = []
+    @correct_letters = []
+    @incorrect_letters = []
+    @guesses = 0
   end
 
   def new_game
     puts "Welcome to Hangman; the fun game where each wrong guess puts a (stick) man closer to his death. \nDo you need a reminder of the rules? (Y/N)"
     rules_needed = gets.chomp.upcase
     self.print_rules if rules_needed[0] == "Y"
-    # call display to show length of word
-    puts "Enter a letter to make your guess:"
-    self.make_guess
+    until @incorrect_letters.size > 6 
+        self.new_round
+        if @display.correct_letters == @secret
+          puts "Congratulations! You won! You saved an innocent man from the gallows and for that your name will forever be honoured."
+          exit!
+        end
+    end
+    puts "Commiserations fine player, I'm sure he was guilty anyway..."
   end
 
   def print_rules
@@ -21,6 +33,15 @@ class Game
     until gets == "\n"
       sleep(5)
     end
+  end
+
+  def new_round
+    # binding.pry
+    puts "Enter a letter to make your guess:"
+    self.make_guess
+    self.check_guess
+    @display.update_correct_letters(@secret, @correct_letters)
+    @guesses += 1
   end
 
   def make_guess
@@ -33,12 +54,22 @@ class Game
       @current_guess = @current_guess[0]
       puts "Hmm, you tried to add too many letters to your guess (cheater or simple mistake?)... \nTaking just the first letter, your guess was '#{@current_guess}'."
     end
-    if @guessed.include?(@current_guess)
+    if @correct_letters.include?(@current_guess) || @incorrect_letters.include?(@current_guess)
       puts "Looks like you've already guessed that. Let's try something else:"
       self.make_guess
+    end
+  end
+
+  def check_guess
+    if @secret.split("").include?(@current_guess)
+      puts "Correct! #{@current_guess} appears in the secret word."
+      @correct_letters << @current_guess
+    else 
+      puts "Oh dear... #{@current_guess} does not appear in the secret word."
+      @incorrect_letters << @current_guess
     end
   end
 end
 
 game = Game.new()
-puts game.new_game
+game.new_game
